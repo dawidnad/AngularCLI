3070edbd310296d0eaa1ad0a9bbaca1d0ce5e273
danad1 <danad1@LCJ-5CG5210FN8>
2018-03-13 10:39:46 +0100

    Form Validation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import { Employee } from '../models/employee.module';
 
 @Component({
   selector: 'home',
@@ -6,4 +7,24 @@ import {Component} from '@angular/core';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+  languages = ['English', 'Spain', 'Polish', 'Other']
+  model = new Employee('', '', true, '', 'default');
+  hasPrimaryLanguageError = false;
+
+  firstNameToUpperCase(value: string){
+    if (value.length > 0) {
+      this.model.firstName = value.charAt(0).toUpperCase() + value.slice(1);
+    }else{
+      this.model.firstName = value;
+    }
+  }
+  validatePrimaryLanguage(value){
+    if(value === 'default'){
+      this.hasPrimaryLanguageError = true;
+
+    }else{
+      this.hasPrimaryLanguageError = false;
+    }
+    console.log('lang ' + !this.hasPrimaryLanguageError );
+  }
 }
